3ec8084e81fe35b5a839e843ed285a9cbc269302
Tim Jones <mynameistim97@gmail.com>
2017-10-17 17:08:44 +1000

    Fixed fullpage.js not working with first slide when transitioning from larger screen to smaller screen

--- files/js/script.js
+++ files/js/script.js
@@ -33,7 +33,38 @@ var safariOrEdge = false;
       //     .removeAttr( 'style' );
       // } );
     })();
+(function() {
+      $( document )
+        .on( "mousemove", "#banner-mb", function( event ) {
 
+        var elmnt = document.getElementById("parallax-mb");
+
+        var halfW = ( elmnt.clientWidth / 2 );
+        var halfH = ( elmnt.clientHeight / 2 );
+        var coorX = ( halfW - ( event.pageX - elmnt.offsetLeft ) );
+        var coorY = ( halfH - ( event.pageY - elmnt.offsetTop ) );
+        var degX, degY;
+      if($(window).width() > 2000 && $(window).height() > 1000){
+        degX  = ( ( coorY / halfH ) * 20 ) + 'deg'; // max. degree = 10
+      }
+      if(safariOrEdge && $(window).width() > 2000 && $(window).height() > 1000){
+        degX  = ( ( coorY / halfH ) * 12 ) + 'deg'; // max. degree = 10
+      }
+      else {
+        degX  = ( ( coorY / halfH ) * 12 ) + 'deg'; // max. degree = 10
+      }
+        degY  = ( ( coorX / halfW ) * -10 ) + 'deg'; // max. degree = 10
+        $('.card-img-mb').css( 'transform', function() {
+
+          return 'perspective( 1200px ) translate3d( 0, 0px, 285px ) rotateX('+ degX +') rotateY('+ degY +')';
+        } );
+      } )
+      //   .on( "mouseout", ".container", function() {
+      //   $('.card').removeAttr( 'style' )
+      //     .children( '.card__summary' )
+      //     .removeAttr( 'style' );
+      // } );
+    })();
 (function() {
   $( document )
     .on( "mousemove", "#card", function( event ) {
@@ -111,6 +142,9 @@ function checkSafariOrEdge() {
   }
 }
 var controller;
+var scenes = [];
+var activeScenes = [];
+
 var smScene;
 var wipeAnimation;
 var parallax;
@@ -119,12 +153,14 @@ var parallax3;
 var parallax4;
 var parallax5;
 var parallax6;
+var parallax7;
 var scene;
 var scene2;
 var scene3;
 var scene4;
 var scene5;
 var scene6;
+var scene7;
 
 var scrollMagicEnabled = false;
 var fullPageCreated = false;
@@ -148,55 +184,58 @@ $( document ).ready(function() {
   if ($( window ).width() < 991) {
         $(".section").css("padding-left", "20px");
         $(".section").css("padding-right", "20px");
-    createFullpage();
-        $(".panel-lg .container").css("width", $(window).innerWidth() -40);
+        createFullpage(); 
+      $(".panel-lg .container").css("width", $(window).innerWidth() -40);
       $(".container").css("height", $(window).innerHeight() - 55);
   }
   else {
     // destroyScrollMagic();
-    // addScrollMagic();
-    $(".panel-lg .container").css("width", $(window).innerWidth() - 120);
+    // ollMagic();
+    if ($( window ).width() > 2000 ) {
+      $(".line-horizontal").css("margin-top", ($(window).innerHeight() / 2.25 ) * -1);
+    }
+    else {
+      $(".line-horizontal").css("top", "0px");
+      $(".line-horizontal").css("margin-top", ($(window).innerHeight() / 2 )* -1);
+    }
+    // $(".panel-lg .container").css("width", $(window).innerWidth() - 120);
+  
     // $(".panel").css("margin-left", $(".dots-horizontal").width());
     scrollMagicEnabled = true;
     wipeAnimation = new TimelineMax()
+      // animate to first panel
       .to("#slideContainer", 0.5, {delay: -0.5})
       // animate to second panel
-      // .to("#slideContainer", 0.5, {z: -150})    // move back in 3D space
       .to("#slideContainer", 1,   {x: "-33.333333%"})  // move in to first panel
-      // .to("#slideContainer", 0.5, {z: 0})       // move back to origin in 3D space
       // animate to third panel
       .to("#slideContainer", 1,   {x: "-50%"})
-      // .to("#slideContainer", 0.5, {z: 0})
       // animate to forth panel
       .to("#slideContainer", 1,   {x: "-66.666666%"})
-      // .to("#slideContainer", 0.5, {z: 0})
       // animate to fifth panel
       .to(".panel-lg", 0.5, {z: 0, delay: 0})
-      // .to(".panel-lg", 1,   {x: "0%"})
-      // .to(".panel-lg", 0.5, {z: 0});
 
     controller = new ScrollMagic.Controller();  
 
-    
     smScene = new ScrollMagic.Scene({
-      triggerElement: "#pinContainer",
-      triggerHook: "onLeave",
-      duration: "450%"
+    triggerElement: "#pinContainer",
+    triggerHook: "onLeave",
+    duration: "450%"
     })
     .setPin("#pinContainer")
     // .addIndicators() // add indicators (requires plugin)
     .setTween(wipeAnimation)
     .addTo(controller);
+    
   }
 
   
 
-  if(!safariOrEdge){
+  if(!safariOrEdge && $(window).width() > 991){
     $('.card-img').addClass('touch');
   }
   else{
     $('.card-img').removeClass('touch');
-    // $('.card-img').addClass('fixed');
+    $('.card-img').addClass('fixed');
     $('.bg').addClass('no-parallax');
   }
   resizePanels();
@@ -207,9 +246,13 @@ $(window).resize(function () {
       // $('#pinContainer').css("height", ($(window).innerHeight() * 5));
       $(".section").css("padding-left", "20px");
       $(".section").css("padding-right", "20px");
+      $(".container").css("width", $(window).innerWidth() -40);
       $(".panel-lg .container").css("width", $(window).innerWidth() -40);
       $(".container").css("height", $(window).innerHeight() - 55);
-    createFullpage();
+        if( scrollMagicEnabled == true ){
+          destroyScrollMagic();
+        }
+      createFullpage();
     }
     else {
         $(".panel-lg .container").css("width", $(window).innerWidth() - 120);
@@ -220,33 +263,37 @@ $(window).resize(function () {
         }
         if( scrollMagicEnabled == false ){
         wipeAnimation = new TimelineMax()
-      // animate to second panel
-      .to("#slideContainer", 0.5, {z: -150})    // move back in 3D space
-      .to("#slideContainer", 1,   {x: "-25%"})  // move in to first panel
-      .to("#slideContainer", 0.5, {z: 0})       // move back to origin in 3D space
-      // animate to third panel
-      .to("#slideContainer", 0.5, {z: -150, delay: 1})
-      .to("#slideContainer", 1,   {x: "-50%"})
-      .to("#slideContainer", 0.5, {z: 0})
-      // animate to forth panel
-      .to("#slideContainer", 0.5, {z: -150, delay: 1})
-      .to("#slideContainer", 1,   {x: "-75%"})
-      .to("#slideContainer", 0.5, {z: 0});
+          // animate to first panel
+          .to("#slideContainer", 0.5, {delay: -0.5})
+          // animate to second panel
+          .to("#slideContainer", 1,   {x: "-33.333333%"})  // move in to first panel
+          // animate to third panel
+          .to("#slideContainer", 1,   {x: "-50%"})
+          // animate to forth panel
+          .to("#slideContainer", 1,   {x: "-66.666666%"})
+          // animate to fifth panel
+          .to(".panel-lg", 0.5, {z: 0, delay: 0})
 
-      controller = new ScrollMagic.Controller();  
-      smScene = new ScrollMagic.Scene({
-          triggerElement: "#pinContainer",
-        triggerHook: "onLeave",
-        duration: "450%"
-      })
-      .setPin("#pinContainer")
-      .setTween(wipeAnimation)
-      .addTo(controller);
+          controller = new ScrollMagic.Controller();  
+          smScene = new ScrollMagic.Scene({
+            triggerElement: "#pinContainer",
+            triggerHook: "onLeave",
+            duration: "450%"
+          })
+          .setPin("#pinContainer")
+          .setTween(wipeAnimation)
+          .addTo(controller);
         }
       // destroyScrollMagic();
       // addScrollMagic();
-      $(".container").css("height", "100%");
       // $('#pinContainer').css("width", $(window).innerWidth() * 4);
+    }
+    if ($( window ).width() > 2000 ) {
+      $(".line-horizontal").css("margin-top", ($(window).innerHeight() / 2.25 ) * -1);
+    }
+    else {
+      $(".line-horizontal").css("top", "0px");
+      $(".line-horizontal").css("margin-top", ($(window).innerHeight() / 2 )* -1);
     }
     onWindowResizeRecreateParallax();
     resizePanels();
@@ -263,7 +310,10 @@ function setupParallax() {
   $('.bg-3').attr('data-depth', '0.5');
   $('.bg-4').attr('data-depth', '0.5');
   $('.bg-5').attr('data-depth', '0.5');
+  $('.bg-6').attr('data-depth', '0.5');
+  $('.bg-7').attr('data-depth', '0.5');
   $('.card').attr('data-depth', '0.0');
+  $('.card-mb').attr('data-depth', '0.0');
 
   // Turn on parallax for elements on desktop
   if ($( window ).width() > 991) {
@@ -282,20 +332,24 @@ function setupParallax() {
     $('.languages-block').attr('data-depth', '0.0');
     $('#interactive-anim').attr('data-depth', '0.0');
   }
-    if(safariOrEdge == false){
-      scene = document.getElementById('scene');
-      parallax = new Parallax(scene);
-    } 
-    scene2 = document.getElementById('scene-2');
-    parallax2 = new Parallax(scene2);
-    scene3 = document.getElementById('scene-3');
-    parallax3 = new Parallax(scene3);
-    scene4 = document.getElementById('scene-4');
-    parallax4 = new Parallax(scene4);
-    scene5 = document.getElementById('scene-5');
-    parallax5 = new Parallax(scene5);       
-    scene6 = document.getElementById('scene-6');
-    parallax6 = new Parallax(scene6);       
+  if(safariOrEdge == false){
+    scene = document.getElementById('scene');
+    parallax = new Parallax(scene);
+  } 
+  scene2 = document.getElementById('scene-2');
+  parallax2 = new Parallax(scene2);
+  scene3 = document.getElementById('scene-3');
+  parallax3 = new Parallax(scene3);
+  scene4 = document.getElementById('scene-4');
+  parallax4 = new Parallax(scene4);
+  scene5 = document.getElementById('scene-5');
+  parallax5 = new Parallax(scene5);       
+  scene6 = document.getElementById('scene-6');
+  parallax6 = new Parallax(scene6);       
+  if($(window).width() < 991){
+    scene7 = document.getElementById('scene-7');
+    parallax7 = new Parallax(scene7);     
+  }
   // }
   // else {
     // scene6 = document.getElementById('scene-6');
@@ -313,7 +367,7 @@ function setupParallax() {
 function createFullpage() {
     if (fullPageCreated == false) {
         fullPageCreated = true;
-    $('#slideContainer').fullpage(options);
+        $('#section-wipes').fullpage(options);
     }
 }
 /**
@@ -325,14 +379,12 @@ function resizePanels() {
   console.log('RESIZED');
   if ($( window ).width() >= 991) {
     //Initialise all panels with height respective to device
-    $('.panel').css({
+      $('.panel').css({
           'height':($(window).innerHeight())
       });
+     
       $('.panel-lg').css({
           'height':($(window).innerHeight())
-      });
-      $('#slideContainer').css({
-          'height':($(window).innerHeight() * 4)
       });
     }
     else {
@@ -374,6 +426,8 @@ function onWindowResizeRecreateParallax() {
     parallax5 = null;
     parallax6.destroy();
     parallax6 = null;
+    parallax7 = null;
+   
     setupParallax();
     // $('#slideContainer').fullpage(options);
 }
@@ -390,87 +444,41 @@ function destroyScrollMagic() {
   controller = null;
   console.log('destroyed');
 }
-/**
- * Set/Remove the padding for the fullpage slider responsively
- * @param  {object} slider The slider container
- * @returns  {null}
- */
-function addScrollMagic() {
-  // destroyScrollMagic();
-  wipeAnimation = new TimelineMax()
-  // animate to second panel
-  .to("#slideContainer", 0.5, {z: -150})    // move back in 3D space
-  .to("#slideContainer", 1,   {x: "-25%"})  // move in to first panel
-  .to("#slideContainer", 0.5, {z: 0})       // move back to origin in 3D space
-  // animate to third panel
-  .to("#slideContainer", 0.5, {z: -150, delay: 1})
-  .to("#slideContainer", 1,   {x: "-50%"})
-  .to("#slideContainer", 0.5, {z: 0})
-  // animate to forth panel
-  .to("#slideContainer", 0.5, {z: -150, delay: 1})
-  .to("#slideContainer", 1,   {x: "-75%"})
-  .to("#slideContainer", 0.5, {z: 0})
-  // animate to fifth panel
-  .to(".panel-lg", 0.5, {z: 0, delay: 0})
-  .to(".panel-lg", 1,   {x: "0%"})
-  .to(".panel-lg", 0.5, {z: 0});
 
-  controller = new ScrollMagic.Controller();  
-  smScene = new ScrollMagic.Scene({
-      triggerElement: "#pinContainer",
-    triggerHook: "onLeave",
-    duration: "450%"
-  })
-  .setPin("#pinContainer")
-  .setTween(wipeAnimation)
-  .addTo(controller);
-}   
-var controller = new ScrollMagic.Controller();
-var scenes = [];
-var activeScenes = [];
-
-scenes.push(function(){
-return new ScrollMagic.Scene({
-  triggerElement: '#pinContainer',
-  triggerHook: 'onLeave'
-})
-.setPin('#pin')
-.addTo(controller);
-});
 
 function addScenes(newScenes) {
-// reset active scenes
-activeScenes = [];
-// loop over each scene and add/re-add
-newScenes.forEach(function (newScene, index) {
-  if (typeof newScene === 'function') {
-    // add the new scene
-    var newScene = newScene();
-    // push it to our active scenes array
-    activeScenes.push(newScene);
-  }
-});
+  // reset active scenes
+  activeScenes = [];
+  // loop over each scene and add/re-add
+  newScenes.forEach(function (newScene, index) {
+    if (typeof newScene === 'function') {
+      // add the new scene
+      var newScene = newScene();
+      // push it to our active scenes array
+      activeScenes.push(newScene);
+    }
+  });
 }
 
 // debounced re-size event - less destroying and re-adding scenes
 var timeoutDuration = 400;
 var resizeTimeout = null;
 window.addEventListener('resize', function(event) {
-if (resizeTimeout) {
-  clearTimeout(resizeTimeout);
-}
-resizeTimeout = setTimeout(function() {
-  // loop over each active scene
-  activeScenes.forEach(function (scene, index) {
-    // make sure scene wasn't null
-    if (scene) {
-      // destroy active scene
-      scene.destroy(true);
-    }
-  });
-  // after we have destroyed old scenes, re-add them
-  addScenes(scenes);
-}, timeoutDuration);
+  if (resizeTimeout) {
+    clearTimeout(resizeTimeout);
+  }
+  resizeTimeout = setTimeout(function() {
+    // loop over each active scene
+    activeScenes.forEach(function (scene, index) {
+      // make sure scene wasn't null
+      if (scene) {
+        // destroy active scene
+        scene.destroy(true);
+      }
+    });
+    // after we have destroyed old scenes, re-add them
+    addScenes(scenes);
+  }, timeoutDuration);
 });
 
 // add initial scenes
@@ -482,7 +490,6 @@ $(document).on('click','.navbar-collapse.in',function(e) {
       $(this).collapse('hide');
   }
 });
-
 var width = 100,
     perfData = window.performance.timing, // The PerformanceTiming interface represents timing-related performance information for the given page.
     EstimatedTime = -(perfData.loadEventEnd - perfData.navigationStart),
