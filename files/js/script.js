d4c226c4550b31aa0498c3bc19d6824820e04566
Tim Jones <mynameistim97@gmail.com>
2019-12-29 13:44:34 +1000

    Design changes to animations

--- files/js/script.js
+++ files/js/script.js
@@ -24,7 +24,7 @@ function preventScrollOnMenuOpen(){
 function applyEffects(element){
   $(".portfolio-image").not(element).css("-webkit-filter", "blur(5px)");
   $(".portfolio-image").not(element).css("filter", "blur(5px)");
-  $(".portfolio-image").not(element).css("opacity", "0.8");
+  $(".portfolio-image").not(element).css("opacity", "0.6");
   // element.parent().css("transform", "scale(1.05)");
   // $(".portfolio-image").parent().not(element.parent()).css("transform", "scale(0.95)");
   $(".portfolio-image").not(element).next().css("opacity", "0.3");
@@ -43,7 +43,7 @@ function hoverEffects() {
   $(".brendan").parent().hover(
     function() {
       $(".portfolio-title").stop().animate({'opacity': 0}, 200, function(){
-        $(".portfolio-title").text("Brisbane Wedding Celebrant").animate({'opacity': 1}, 200);
+        $(".portfolio-title").text("Brendan Anning Wedding Celebrant").animate({'opacity': 1}, 200);
       });
       applyEffects($(this).find(".portfolio-image"));
     },
@@ -57,7 +57,7 @@ function hoverEffects() {
   $(".aml").parent().hover(
     function() {
       $(".portfolio-title").stop().animate({'opacity': 0}, 200, function(){
-        $(".portfolio-title").text("Melbourne Asset Management").animate({'opacity': 1}, 200);
+        $(".portfolio-title").text("AML Asset Management").animate({'opacity': 1}, 200);
       });
       applyEffects($(this).find(".portfolio-image"));
     },
@@ -71,7 +71,7 @@ function hoverEffects() {
   $(".tla").parent().hover(
     function() {
       $(".portfolio-title").stop().animate({'opacity': 0}, 200, function(){
-        $(".portfolio-title").text("Government and Defence Logistics Management").animate({'opacity': 1}, 200);
+        $(".portfolio-title").text("TLA Logistics Management").animate({'opacity': 1}, 200);
       });
       applyEffects($(this).find(".portfolio-image"));
     },
@@ -85,7 +85,7 @@ function hoverEffects() {
   $(".qutrunning").parent().hover(
     function() {
       $(".portfolio-title").stop().animate({'opacity': 0}, 200, function(){
-        $(".portfolio-title").text("Running Club of QUT").animate({'opacity': 1}, 200);
+        $(".portfolio-title").text("QUT Running Club").animate({'opacity': 1}, 200);
       });
       applyEffects($(this).find(".portfolio-image"));
     },
@@ -99,7 +99,7 @@ function hoverEffects() {
   $(".quteb").parent().hover(
     function() {
       $(".portfolio-title").stop().animate({'opacity': 0}, 200, function(){
-        $(".portfolio-title").text("Exchange Club of QUT").animate({'opacity': 1}, 200);
+        $(".portfolio-title").text("QUT Exchange Club").animate({'opacity': 1}, 200);
       });
       applyEffects($(this).find(".portfolio-image"));
     },
@@ -113,7 +113,7 @@ function hoverEffects() {
   $(".daryl").parent().hover(
     function() {
       $(".portfolio-title").stop().animate({'opacity': 0}, 200, function(){
-        $(".portfolio-title").text("Brisbane Entertainer").animate({'opacity': 1}, 200);
+        $(".portfolio-title").text("Daryl Murphy Brisbane Entertainer").animate({'opacity': 1}, 200);
       });
       applyEffects($(this).find(".portfolio-image"));
     },
@@ -127,7 +127,7 @@ function hoverEffects() {
   $(".aaron").parent().hover(
     function() {
       $(".portfolio-title").stop().animate({'opacity': 0}, 200, function(){
-        $(".portfolio-title").text("Brisbane Entertainer and DJ").animate({'opacity': 1}, 200);
+        $(".portfolio-title").text("Aaron Maybus DJ").animate({'opacity': 1}, 200);
       });
       applyEffects($(this).find(".portfolio-image"));
     },
@@ -160,7 +160,7 @@ function hoverEffects() {
       $(".food-background").removeClass("active");
     }
   );
-  $(".skill-item").click(function() {
+  $(".skill-item").hover(function() {
     var thisElement = $(this);
     thisElement.css("opacity", "0");
     setTimeout( function(){
@@ -200,8 +200,8 @@ function navbarElementHoverAnim(){
 }
 function addWhiteNav(){
   $(".navbar-nav li a").css("color", "black");
-  $(".navbar-default").css("background-color", "rgba(59, 59, 59, 0.9)");
-  $(".navbar-collapse").css("background-color", "rgba(59, 59, 59, 0.9)");
+  $(".navbar-default").css("background-color", "rgba(0, 0, 0, 0.8)");
+  $(".navbar-collapse").css("background-color", "rgba(0, 0, 0, 0.8)");
   $(".navbar-default").css("border-top", "none");
   $(".navbar-default").css("height", "57px");
 }
@@ -210,16 +210,36 @@ function addTransparentNav(){
   $(".navbar-default").css("background-color", "transparent");
   $(".navbar-default").css("border-top", "none");
   $(".navbar-collapse").css("background-color", "none");
+  // $( ".navbar-nav li a" ).each(function( index ) {
+  //     //Animate the margin of each nav element
+  //     $(this)
+  //     .delay(200)
+  //     .queue(function (next) { 
+  //       $(this).css("margin-top", "40px"); 
+  //       next(); 
+  //     });
+  // });
   $(".navbar-nav li a").css("margin-top", "40px");
   $(".navbar-default").css("height", "100px");
   $(".navbar-nav li a:hover").css("color", "#fff");
   $(".navbar-nav li .navbar-brand").css("margin-top", "25px");
 }
 function addWhiteNavDesktop(){
-  $(".navbar-default").css("background-color", "rgba(59, 59, 59, 0.9)");
+  $(".navbar-default").css("background-color", "rgba(0, 0, 0, 0.8)");
   $(".navbar-default").css("border-top", "none");
+  $(".navbar-default").css("display", "block");
   $(".navbar-collapse").css("background-color", "none");
+  // $( ".navbar-nav li a" ).each(function( index ) {
+  //   //Animate the margin of each nav element
+  //   $(this)
+  //   .delay(200)
+  //   .queue(function (next) { 
+  //     $(this).css("margin-top", "10px"); 
+  //     next(); 
+  //   });
+  // });
   $(".navbar-nav li a").css("margin-top", "10px");
+  $(".navbar-nav li a").css("opacity", "1");
   $(".navbar-default").css("height", "75px");
   $(".navbar-nav li .navbar-brand").css("margin-top", "-5px");
 }
@@ -234,7 +254,7 @@ function animateNavbar(){
   else {
     //Scroll position is in About section
     if($(document).scrollTop() > 1) {
-      addWhiteNavDesktop();
+      addWhiteNavDesktop();     
     }
     //Scroll position is in Main section
     else {
@@ -307,7 +327,8 @@ function createGoTopArrow(){
 //Reset the elements that require resizing
 function setMainElements(){
   $(".main").css("height", $(window).height() + "px");
-
+  $(".navbar-nav li a").removeAttr("style"); 
+  
   //If the window is a square and not rectangular
   if($(window).width() < $(window).height() * 1.7) {
     $("video").css("height", "100vh");
@@ -321,13 +342,8 @@ function setMainElements(){
   //Reset all styles on navbar if on desktop width
   if($(window).width() > 767) {
     $(".navbar-default").removeAttr("style");
-    // $(".navbar-nav li a").removeAttr("style");
-    $( ".navbar-nav li a" ).each(function( index ) {
-      //Set each of the underline to the width of each nav element text
-      $(this).next().css('max-width', $(this).width() + 'px');
-    });
+    // $(".navbar-nav li a").removeAttr("style");   
     $(".navbar-collapse").removeAttr("style");
-    $(".ribbon").css("animation", "none");
     $(".navbar-collapse .navbar-nav").removeClass("transparent");
   }
   else {
@@ -335,103 +351,103 @@ function setMainElements(){
       $(this).css('width', '100%');
     });
   }
-
-  var delay = 1200;
-
-  //Check if the page has only loaded once, if yes then set elements after 1000 ms
-  if(!firstTime){
-    //If page has already loaded and this is new execution, set delay as 400 ms
-    delay = 250;
-  }
-  //Debounce the height change function for the portfolio items and navbar animations
-  setTimeout(function(){
-    animateNavbar();
-    $(".ribbon").css("top", "50%");
-    $(".main").css("height", $(window).innerHeight());
-
-    if($(window).width() < 991) {
-      $(".title-text").css("top", "53%");
-      $(".title-text").css("transform", "translate(0%, -53%");
-    }
-    else {
-      $(".title-text").css("top", "49%");
-      $(".title-text").css("transform", "translate(0%, -49%");
-    }
-    if($(window).width() < 767) {
-      if($(window).height() > 450) {
-        $(".title-text").css("top", "54%");
-      }
-      //For VERY long phones in landscape 
-      else if($(window).height() < 300) {
-        $(".title-text").css("top", "52%");
-      }
-      else {
-        $(".title-text").css("top", "58%");
-        $(".title-text").css("transform", "translate(0%, -58%");
-      }
-      //If not first time this function is executed, apply styles 
-      if(!firstTime){
-        $(".navbar-collapse .navbar-nav").removeClass("opaque");
-        $(".navbar-collapse .navbar-nav").addClass("transparent");
-      }
-    }
-    if($(window).width() < 479){
-      $(".title-text").css("top", "59%");
-      $(".title-text").css("transform", "translate(0%, -59%");
-    }
-    else{
-      if(!firstTime){
-        $(".navbar-collapse .navbar-nav").addClass("opaque");
-        $(".navbar-collapse .navbar-nav").removeClass("transparent");
-      }
-    }
-    //Set firstTime as false after first execution of function
-    firstTime = false;
-  }, delay);
-  setTimeout(function(){
-    $(".navbar-default").css("z-index", "103");
-  }, delay);
 }
 
 $(window).resize(function () { 
   // console.log('RESIZED'); 
   $(".bars").removeClass("active");
   setMainElements();
+  // if($(document).scrollTop() == 0){
+  //   $(".navbar-nav li a").css("margin-top", "40px");
+  //   $(".navbar-nav li .navbar-brand").css("margin-top", "25px");
+  // }
+  // else {
+  //   $(".navbar-nav li a").css("margin-top", "10px");
+  //   $(".navbar-nav li .navbar-brand").css("margin-top", "-5px");
+  // }
+  $(".navbar-default").css('display', 'block');
+  $(".navbar-nav li").css('opacity', '1');
+  $(".navbar-nav li").css('animation-name', 'unset');
+  $(".navbar-nav li").css('-webkit-animation-name', 'unset');
+  animateNavbar();
+  $( ".navbar-nav li a" ).each(function( index ) {
+    //Set each of the underline to the width of each nav element text
+    $(this).next().css('max-width', $(this).width() + 'px');
+  });
 });
 
 function createScrollRevealEffects(){
-  var slideInConfig = {
-    origin: 'bottom',
-    duration: 700,
-    distance: '400px',
-    viewOffset: {
-      bottom: 250
-    },
-    //If effect has already occured, remove styles applied by scrollReveal
-    //to allow for hover transform effects to still occur
-    afterReveal: function() {
-      setTimeout(function(){
-        $('.portfolio-image').removeAttr("style");
-        //Reset the height again after removal
-        hoverEffects();
-      }, 1000);
+  var slideInConfig;
+  var fadeInConfig;
+  if($(window).width() > 767) {
+    slideInConfig = {
+      origin: 'bottom',
+      duration: 800,
+      distance: '400px',
+      scale: '0.5',
+      viewOffset: {
+        bottom: 700
+      },
+      //If effect has already occured, remove styles applied by scrollReveal
+      //to allow for hover transform effects to still occur
+      afterReveal: function() {
+        setTimeout(function(){
+          $('.portfolio-image').removeAttr("style");
+          //Reset the height again after removal
+          hoverEffects();
+        }, 1000);
+      }
     }
   }
-  var fadeInConfig = {
-    duration: 700,
-    origin: 'bottom',
-    distance: '400px',
-    viewOffset: {
-      top: 250
-    },
-    afterReveal: function() {
-      setTimeout(function(){
-        $('.skill-item').removeAttr("style");
-        $('.portfolio-item').removeAttr("style");
-        hoverEffects();
-      }, 400);
+  else {
+    slideInConfig = {
+      origin: 'bottom',
+      duration: 600,
+      distance: '400px',
+      scale: '0.9',
+      afterReveal: function() {
+        setTimeout(function(){
+          $('.portfolio-image').removeAttr("style");
+          //Reset the height again after removal
+          hoverEffects();
+        }, 1000);
+      }
     }
   }
+  if($(window).width() > 767) {
+    fadeInConfig = {
+      duration: 800,
+      origin: 'bottom',
+      distance: '400px',
+      scale: '0.5',
+      viewOffset: {
+        bottom: 700
+      },
+      afterReveal: function() {
+        setTimeout(function(){
+          $('.skill-item').removeAttr("style");
+          $('.portfolio-item').removeAttr("style");
+          hoverEffects();
+        }, 400);
+      }
+    }
+  }
+  else {
+    fadeInConfig = {
+      duration: 1000,
+      origin: 'bottom',
+      distance: '400px',
+      scale: '0.5',
+      afterReveal: function() {
+        setTimeout(function(){
+          $('.skill-item').removeAttr("style");
+          $('.portfolio-item').removeAttr("style");
+          hoverEffects();
+        }, 600);
+      }
+    }
+  }
+
   window.sr = ScrollReveal();
 
   sr.reveal('.portfolio-item', slideInConfig);
@@ -468,13 +484,16 @@ function bindVelocity(){
       // set target to anchor's "href" attribute
       var target = $(this).attr('href');
 
-      if(target == "#home" || target == "#about" || target == "#portfolio" 
-        || target == "#skills" || target == "#experience" || 
+      if(target == "#home" || target == "#about" || target == "#portfolio" ||
+        target == "#portfolio2" || target == "#skills" || target == "#experience" || 
         target == "#contact" || target == "#about2") {
 
         //If arrow up or arrow down pressed, convert ID to actual ID instead of toggling menu bar active class
         if(target == "#about2"){
           target = "#about";
+        }
+        else if (target == "#portfolio2"){
+          target = "#portfolio";
         }
         else {
           if($(window).width() < 767){
@@ -492,62 +511,6 @@ function bindVelocity(){
       }
   });
 }
-function destroyScrollMagic() {
-  wipeAnimation = null;
-  smScene.destroy();
-  smScene = null;
-  controller.destroy();
-  controller = null;
-  console.log('destroyed');
-}
-
-var controller = new ScrollMagic.Controller();
-var scenes = [];
-var activeScenes = [];
-
-scenes.push(function(){
-return new ScrollMagic.Scene({
-  triggerElement: '#pinContainer',
-  triggerHook: 'onLeave'
-})
-.setPin('#pin')
-.addTo(controller);
-});
-
-function addScenes(newScenes) {
-// reset active scenes
-activeScenes = [];
-// loop over each scene and add/re-add
-newScenes.forEach(function (newScene, index) {
-  if (typeof newScene === 'function') {
-    // add the new scene
-    var newScene = newScene();
-    // push it to our active scenes array
-    activeScenes.push(newScene);
-  }
-});
-}
-
-// debounced re-size event - less destroying and re-adding scenes
-var timeoutDuration = 400;
-var resizeTimeout = null;
-window.addEventListener('resize', function(event) {
-if (resizeTimeout) {
-  clearTimeout(resizeTimeout);
-}
-resizeTimeout = setTimeout(function() {
-  // loop over each active scene
-  activeScenes.forEach(function (scene, index) {
-    // make sure scene wasn't null
-    if (scene) {
-      // destroy active scene
-      scene.destroy(true);
-    }
-  });
-  // after we have destroyed old scenes, re-add them
-  addScenes(scenes);
-}, timeoutDuration);
-});
 
 Pace.restart();
 Pace.on("done", function(){
@@ -555,62 +518,31 @@ Pace.on("done", function(){
       $('.preloader-wrap').fadeOut(1000);
       $(".background-wrapper").attr("data-anim","true");
       $('.background-wrapper').addClass("active");
+      if($(window).width() < 767){
+        $('.navbar-default').css("display", "block");
+        $('.navbar-nav li a').css('opacity', '1');
+      }
+      else {
+        setTimeout(function(){
+          $('.navbar-default').css("display", "block");
+        },3000);
+        setTimeout(function(){
+          $( ".navbar-nav li a" ).each(function( index ) {
+            //Set each of the underline to the width of each nav element text
+            $(this).next().css('max-width', $(this).width() + 'px');
+          });
+        },3100);
+      }
       setTimeout(function(){
-        $('.fade').css("margin-bottom", "0px");
-        $('.fade').css("opacity", "1");
+        $('.title-text').css("display", "block");
       },1000);
       setTimeout(function(){
-        $('.fade-second').css("opacity", "1");
-        $('.fade-second').css("margin-bottom", "0px")
-      },1300);
-      setTimeout(function(){
-        $('.view-work').css("opacity", "1");
-        $('.view-work').css("margin-bottom", "0px")
-      },1600);
-      setTimeout(function(){
         $('.highlight-red').addClass('active');
-      },2000);
+      },2100);
       setTimeout(function(){
         $('.highlight-purple').addClass('active');
       },2400);
-      // $('.ribbon').css("-webkit-animation", "slide 0.8s forwards");
-      var counter = 0;
-
-      //Only activate once
-      $(window).scroll(function() { 
-        if($(document).scrollTop() > 20 && $(window).width() > 767 && counter < 1){
-          $(".navbar-default").addClass("opaque");
-          $(".navbar-collapse .navbar-nav").addClass("opaque");
-          $(".arrow").addClass("opaque");
-          counter = 1;
-        }
-      });
-
-      
-
-    var delay = 4200;
-    if($(document).scrollTop() > 10 || $(window).width() < 767) {
-      if($(window).width() < 767 && $(document).scrollTop() > 10){
-        //If on mobile and the screen is past 10px scroll position, 
-        //make the arrow opaque instead of waiting for fade in animation
-        $(".arrow").addClass("opaque");
-      }
-      $(".fade-last").css("opacity", "1");
-    }
-    else {    
-      setTimeout(function(){
-        $(".navbar-default").css("opacity", "1");
-        $(".arrow").css("opacity", "1");
-      }, 3200);
-
-      
-    }
-    //Less delay for arrow to fade in if on mobile
-    if($(window).width() < 767) {
-      delay = 3200;
-    }
   } 
-    
 });
 function fadeOutVideo(){
   var height = ($(window).height() / 1.5);
@@ -625,8 +557,6 @@ $( document ).ready(function() {
   }
   $(".bars").removeClass("active");
   
-  // add initial scenes
-  addScenes(scenes);
   setMainElements();
   hoverEffects();
   createGoTopArrow();
@@ -638,9 +568,10 @@ $( document ).ready(function() {
   $(window).scroll(function() { 
     animateNavbar();   
     preventScrollOnMenuOpen();
-    // fadeOutVideo();
   });
-
+  if($(document).scrollTop() > 0){
+    $(".navbar-default").css('opacity', '1');
+  }
   var theDate = new Date(); 
   $(".year").text(theDate.getFullYear());
 });
