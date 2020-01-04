5a7777c4fbfa30728c655952e5af8ec8ee3d8ce6
Tim Jones <mynameistim97@gmail.com>
2020-01-04 20:26:25 +1000

    Uploaded background to Isuru site

--- isuru/script.js
+++ isuru/script.js
@@ -4,14 +4,12 @@ $( document ).ready(function() {
   if($(window).width() < 767){
     $(".cracker").hover(
       function() {
-        $(".touch-message").stop().animate({'opacity': 0}, 200, function(){
-          $(".touch-message").text("Close egg").animate({'opacity': 1}, 200);
-        });
+        $(".touch-message").text("Close egg").animate({'opacity': 1}, 100);
+        $(".background").addClass('active')
       },
       function() {
-        $(".touch-message").stop().animate({'opacity': 0}, 200, function(){
-          $(".touch-message").text("Tap the egg!").animate({'opacity': 1}, 200);
-        });
+          $(".touch-message").text("Tap the egg!").animate({'opacity': 1}, 100);
+        $(".background").removeClass('active')
       }
     );
   }
@@ -19,9 +17,11 @@ $( document ).ready(function() {
   $(".cracker").hover(
       function() {
         $(".touch-message").css("opacity", "0");
+        $(".background").addClass('active')
       },
       function() {
         $(".touch-message").css("opacity", "1");
+        $(".background").removeClass('active')
       }
     );
   }
@@ -30,5 +30,5 @@ $( document ).ready(function() {
 $(window).resize(function () { 
   $('body').css('height', $(window).height() + 'px');
   $('body').css('width', $(window).width() + 'px');
-  
+
 });
