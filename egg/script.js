e5b2e9e719bba1878051bfd3a2e307e053483cf4
Tim Jones <mynameistim97@gmail.com>
2020-01-04 22:42:41 +1000

    Added background to button on Egg site

--- egg/script.js
+++ egg/script.js
@@ -5,13 +5,15 @@ $( document ).ready(function() {
     $(".cracker").hover(
       function() {
         $(".touch-message").text("Close egg").animate({'opacity': 1}, 100);
+        // $(".touch-message").css('background', 'rgba(255,255,255,0.5)');
         $(".background").addClass('active')
         $('.background').css('opacity', '0.5');
       },
       function() {
           $(".touch-message").text("Tap the egg!").animate({'opacity': 1}, 100);
         $(".background").removeClass('active')
-      $('.background').css('opacity', '0');
+        $('.background').css('opacity', '0');
+        // $(".touch-message").css('background', 'none');
       }
     );
   }
