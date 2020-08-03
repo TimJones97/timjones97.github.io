70851e033144ad1848069aa0c3c696f20d5a9ae9
Tim Jones <timothyjones.1@outlook.com>
2020-08-03 11:45:21 +1000

    Added colour to cards

--- files/js/script.js
+++ files/js/script.js
@@ -277,6 +277,61 @@ function hoverEffects() {
   });
 }
 
+function interactiveCursor(){
+  const updateProperties = (elem, state) => {
+    elem.style.setProperty('x', `${state.x}px`)
+    elem.style.setProperty('y', `${state.y}px`)
+    elem.style.setProperty('width', `${state.width}px`)
+    elem.style.setProperty('height', `${state.height}px`)
+    elem.style.setProperty('radius', state.radius)
+    elem.style.setProperty('scale', state.scale)
+    // elem.style.setProperty('transform', 'translate(' + (state.x - state.y / 2) + ')')
+
+    // transform: translate(state.x - state.y / 2), calc(var(--y) - var(--height) / 2));
+  }
+
+  document.querySelectorAll('.cursor').forEach(cursor => {
+    let onElement
+
+    const createState = e => {
+      const defaultState = {
+        x: e.clientX,
+        y: e.clientY,
+        width: 40,
+        height: 40,
+        radius: '50%'
+      }
+
+      const computedState = {}
+
+      if (onElement != null) {
+        const { top, left, width, height } = onElement.getBoundingClientRect()
+        const radius = window.getComputedStyle(onElement).borderTopLeftRadius
+
+        computedState.x = left + width / 2
+        computedState.y = top + height / 2
+        computedState.width = width
+        computedState.height = height
+        computedState.radius = radius
+      }
+
+      return {
+        ...defaultState,
+        ...computedState
+      }
+    }
+
+    document.addEventListener('mousemove', e => {
+      const state = createState(e)
+      updateProperties(cursor, state)
+    })
+
+    document.querySelectorAll('a, button').forEach(elem => {
+      elem.addEventListener('mouseenter', () => (onElement = elem))
+      elem.addEventListener('mouseleave', () => (onElement = undefined))
+    })
+  })
+}
 function navbarElementHoverAnim(){
   $(".navbar-nav li").hover(
     function() {
@@ -684,7 +739,7 @@ function fadeOutVideo(){
 }
 $( document ).ready(function() {
   $(".bars").removeClass("active");
-
+  // interactiveCursor();
   setMainElements();
   hoverEffects();
   createGoTopArrow();
