0c92cc7fdaf7f66982400e71dcf0c4b9475476f9
Tim Jones <t27.jones@connect.qut.edu.au>
2021-04-21 17:10:39 +1000

    Added scrolling text and menu

--- experiment/files/js/script.js
+++ experiment/files/js/script.js
@@ -3,9 +3,45 @@ function writeLiveReload(){
 		document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>');
 	}
 }
-$(document).on("click mousemove",".box",function(e){ 
+function hoverChangeCursorColour(){
+	$('.background-text.orange').hover(
+		function() {
+			$(".circle").addClass("orange");
+	  	}, function() {
+			$(".circle").removeClass("orange");
+		}
+	);
+	$('.background-text.blue').hover(
+		function() {
+			$(".circle").addClass("blue");
+	  	}, function() {
+			$(".circle").removeClass("blue");
+		}
+	);
+	$('.background-text.pink').hover(
+		function() {
+			$(".circle").addClass("pink");
+	  	}, function() {
+			$(".circle").removeClass("pink");
+		}
+	);
+}
+$(document).ready(function(){
+	hoverChangeCursorColour();
+});
+$(document).on("click mousemove","body",function(e){ 
 	var x = e.clientX;
 	var y = e.clientY;
-	var newposX = x - 60;
-	var newposY = y - 60; $(".circle").css("transform","translate3d("+newposX+"px,"+newposY+"px,0px)");
+	var newposX = x - 250;
+	var newposY = y - 250; 
+	if(newposY < 100 && newposX > ($(window).width() - 500)){
+		$(".circle").addClass("tiny");
+		newposX = x;
+		newposY = y; 
+	}
+	else {
+		$(".circle").removeClass("tiny");
+	}
+	$(".circle").css("transform","translate3d("+newposX+"px,"+newposY+"px,0px)");
 });
+
